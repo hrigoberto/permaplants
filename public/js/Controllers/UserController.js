56d774104dbe5ecab23b8c18b0e8cb97b78cd461
jfkbeatkid <hernadezrigoberto@gmail.com>
2016-08-24 19:39:37 -0400

    got signup form on page, fixed app a little bit

--- public/js/Controllers/UserController.js
+++ public/js/Controllers/UserController.js
@@ -2,18 +2,23 @@
   angular.module('permaplants')
          .controller('Auth', Auth);
 
-  Auth.$inject = ['$scope', '$firebaseAuth'];
+  Auth.$inject = ['$scope', '$firebaseAuth' ];
 
   function Auth($scope, $firebaseAuth){
     var auth = $firebaseAuth();
+    // var refUsers = firebase.database().ref('users')
+    // $scope.users = $firebase(refUsers);
     $scope.Login = googleLogin;
     $scope.createUser = createUser;
+
 
 
     function createUser(){
       $scope.message = null;
       $scope.error = null;
-      
+      // $scope.users.$add({
+      //
+      // })
       auth.$createUserWithEmailAndPassword($scope.email, $scope.password)
         .then(function(firebaseUser){
           $scope.message = "User Created with uid:" + firebaseUser.uid;
