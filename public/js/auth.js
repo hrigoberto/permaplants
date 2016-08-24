9040261bb91db2d78e7a6f08674d6cda2ab62c57
jfkbeatkid <hernadezrigoberto@gmail.com>
2016-08-23 20:59:04 -0400

    got the google Oauth working

--- public/js/auth.js
+++ public/js/auth.js
@@ -6,10 +6,11 @@
 
   function Auth($scope, $firebaseAuth){
     var auth = $firebaseAuth();
+    $scope.Login = googleLogin;
 
     function googleLogin(){
-      auth.$signInWithPopup('facebook').then(function(firebaseUser){
-        console.log("Signed in as:", firebaseUser.uid);
+      auth.$signInWithPopup('google').then(function(firebaseUser){
+        console.log("Signed in as:", firebaseUser);
       }).catch(function(error){
         console.log("Authentication failed:", error);
       });
