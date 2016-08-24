4802f8d9f55d48707a210e639b283c2d56bb88ef
jfkbeatkid <hernadezrigoberto@gmail.com>
2016-08-23 22:27:32 -0400

    implemented createuserwithemailandpassword, commented it out

--- public/js/auth.js
+++ public/js/auth.js
@@ -7,7 +7,20 @@
   function Auth($scope, $firebaseAuth){
     var auth = $firebaseAuth();
     $scope.Login = googleLogin;
+    $scope.createUser = createUser;
 
+
+    // function createUser(){
+    //   $scope.message = null;
+    //   $scope.error = null;
+    //   auth.$createUserWithEmailAndPassword($scope.email, $scope.password)
+    //     .then(function(firebaseUser){
+    //       $scope.message = "User Created with uid:" + firebaseUser.uid;
+    //     })
+    //     .catch(function(error){
+    //       $scope.error = error;
+    //     })
+    // }
     function googleLogin(){
       auth.$signInWithPopup('google').then(function(firebaseUser){
         console.log("Signed in as:", firebaseUser);
